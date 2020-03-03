8376cc0be03906f4167646c44a0d842979c26ca8
Caio Vidal <caio.avidal@gmail.com>
2020-03-03 12:28:32 -0300

    Create CountDiv.js

---
+++ CountDiv.js
@@ -0,0 +1,8 @@
+function solution(A, B, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+  
+    let start = A%K == 0 ? A : A + (K - (A%K));
+    let end = B - (B%K);
+    
+    return (end/K) - (start/K) + 1;
+}
